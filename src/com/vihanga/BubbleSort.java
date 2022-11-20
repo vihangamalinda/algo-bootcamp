ed1249319dd1679740c255ee69f7c9e257a52de1
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-11-20 14:05:45 +0530

    Bubble sort implementation.

--- src/com/vihanga/BubbleSort.java
+++ src/com/vihanga/BubbleSort.java
@@ -1,3 +1,22 @@
 package com.vihanga;
+
+import com.vihanga.utils.Utils;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        int [] arr = {12,43,-5,80,-17,13,50};
+
+        for (int lastUnSortedIndex= arr.length-1; lastUnSortedIndex>0;lastUnSortedIndex--){
+            for (int i=0;i<lastUnSortedIndex;i++){
+                if(arr[i]>arr[i+1]){
+                    Utils.swap(arr,i,i+1);
+                }
+            }
+        }
+
+        for (int a :arr){
+            System.out.println(a);
+        }
+
+    }
 }
